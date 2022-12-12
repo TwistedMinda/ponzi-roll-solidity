beb1e23b8f251f34f75d27b2e490f538b54642a2
Twisted <julien.brunet.92@gmail.com>
2022-12-12 18:45:36 +0100

    some tests added

--- test/Game.ts
+++ test/Game.ts
@@ -5,35 +5,43 @@ import { ethers } from "hardhat";
 import { parseEther } from "ethers/lib/utils";
 import { Game } from "../typechain-types";
 import { BigNumber } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 type CurrentRound = Awaited<ReturnType<Game['currentRound']>>
 type LastRound = Awaited<ReturnType<Game['lastRound']>>
 type Stats = Awaited<ReturnType<Game['stats']>>
+type PlayerState = Awaited<ReturnType<Game['players']>>
 
 type Info = {
 	current: CurrentRound,
 	last: LastRound,
 	stats: Stats,
 }
-type GetInfoCallback = (info: Info) => void
+type GetInfoCallback = (info: Info) => Promise<any> | any
+type GetPlayerCallback = (player: PlayerState) => Promise<any> | any
 
 const ROUND_DURATION = 5 * 60
 const GAME_PRICE = parseEther("0.001")
 
-const refetchInfo = async (game: Game, callback: GetInfoCallback) => {
-	callback({
-		current: await game.currentRound(),
-		last: await game.lastRound(),
-		stats: await game.stats(),
-	})
-}
+const _getInfo = async (game: Game, callback: GetInfoCallback) => callback({
+	current: await game.currentRound(),
+	last: await game.lastRound(),
+	stats: await game.stats(),
+})
 
-const _play = (bet: number, game: Game, from: string) => game.play(bet, {
-	from,
-	value: parseEther("0.001"),
+const _getPlayer = async (address: string, game: Game, callback: GetPlayerCallback) => callback(await game.players(address))
+
+const _play = (
+	bet: number,
+	game: Game,
+	account: SignerWithAddress,
+	amount = GAME_PRICE
+) => game.connect(account).play(bet, {
+	from: account.address,
+	value: amount,
 });
-const _claim = (game: Game, from: string) => game.claim({
-	from,
+const _claim = (game: Game, account: SignerWithAddress) => game.connect(account).claim({
+	from: account.address,
 });
 
 const deploy = async () => {
@@ -47,73 +55,115 @@ const deploy = async () => {
 
 describe("Game", function () {
 	
-	it("Should upgrade round", async function () {
-		const { owner, game } = await loadFixture(deploy);
-
-		let timeSnap: BigNumber
-
-		refetchInfo(game, ({ current, last }) => {
-			expect(current.id).equal(1)
-			timeSnap = last.timestamp
-		})
-
-		await time.increase(ROUND_DURATION)
-		await _play(1, game, owner.address)
-
-		refetchInfo(game, ({ current, last }) => {
-			expect(current.id).equal(2)
-			expect(last.timestamp).equal(timeSnap.add(ROUND_DURATION))
-		})
-	})
-
-	it("Winning updates correctly", async function () {
-		const { owner, game } = await loadFixture(deploy);
+	describe('Workflow', () => {
 		
-		refetchInfo(game, ({ current }) => expect(current.benefits).equal(0))
-		await _play(2, game, owner.address)
-		refetchInfo(game, ({ stats, current }) => {
-			expect(stats.totalWinners).equal(1)
-			expect(current.benefits).equal(0)
+		it("Should upgrade round", async () => {
+			const { owner, game } = await loadFixture(deploy);
+	
+			let timeSnap: BigNumber
+	
+			_getInfo(game, ({ current, last }) => {
+				expect(current.id).equal(1)
+				timeSnap = last.timestamp
+			})
+	
+			await time.increase(ROUND_DURATION)
+			await _play(1, game, owner)
+	
+			_getInfo(game, ({ current, last }) => {
+				expect(current.id).equal(2)
+				expect(last.timestamp).equal(timeSnap.add(ROUND_DURATION))
+			})
+		})
+	
+		it("Winning updates correctly", async () => {
+			const { owner, game } = await loadFixture(deploy);
+			
+			_getInfo(game, ({ current }) => expect(current.benefits).equal(0))
+			await _play(2, game, owner)
+			_getInfo(game, ({ stats, current }) => {
+				expect(stats.totalWinners).equal(1)
+				expect(current.benefits).equal(0)
+			})
+		})
+	
+		it("Losing updates correctly", async () => {
+			const { owner, game } = await loadFixture(deploy);
+	
+			_getInfo(game, ({ current }) => expect(current.benefits).equal(0))
+			await _play(1, game, owner)
+			_getInfo(game, ({ current }) => expect(current.benefits).equal(GAME_PRICE))
+		})
+	
+		it("Can claim", async () => {
+			const { owner, game } = await loadFixture(deploy);
+	
+			_getInfo(game, ({ stats }) => expect(stats.totalClaimed).equal(0))
+	
+			await _play(2, game, owner)
+			await _play(1, game, owner)
+			await time.increase(ROUND_DURATION)
+			await _play(1, game, owner)
+			await expect(_claim(game, owner)).to.not.be.reverted
+		
+			_getInfo(game, ({ stats }) => expect(stats.totalClaimed).equal(GAME_PRICE))
 		})
 	})
 
-	it("Losing updates correctly", async function () {
-		const { owner, game } = await loadFixture(deploy);
+	describe('Player', () => {
 
-		refetchInfo(game, ({ current }) => expect(current.benefits).equal(0))
-		await _play(1, game, owner.address)
-		refetchInfo(game, ({ current }) => expect(current.benefits).equal(GAME_PRICE))
+		it("Stats increasing", async () => {
+			const { owner, otherAccount, game } = await loadFixture(deploy);
+			const target = otherAccount
+
+			await _getPlayer(target.address, game, (player) => {
+				expect(player.nbShares).equal(0)
+				expect(player.totalClaimed).equal(0)
+			})
+
+			await _play(2, game, target)
+			await _play(1, game, target)
+			await time.increase(ROUND_DURATION)
+			await _play(1, game, target)
+			await _claim(game, target)
+		
+			await _getPlayer(target.address, game, (player) => {
+				expect(player.nbShares).equal(1)
+				expect(player.totalClaimed).equal(GAME_PRICE)
+			})
+		})
 	})
 
-	it("Can claim", async function () {
-		const { owner, game } = await loadFixture(deploy);
+	describe('Errors', () => {
 
-		await _play(2, game, owner.address)
-		await _play(1, game, owner.address)
-		await time.increase(ROUND_DURATION)
-		await _play(1, game, owner.address)
-		await expect(_claim(game, owner.address)).to.not.be.reverted
+		it("Nothing to claim", async () => {
+			const { owner, game } = await loadFixture(deploy);
+			await _play(2, game, owner)
+			await expect(_claim(game, owner)).to.be.revertedWith('Nothing to claim')
+		})
+	
+		it("No shares", async () => {
+			const { owner, game } = await loadFixture(deploy);
+			await expect(_claim(game, owner)).to.be.revertedWith('You have no share')
+		})
+	
+		it("Already claimed", async () => {
+			const { owner, game } = await loadFixture(deploy);
+			await _play(2, game, owner)
+			await _play(1, game, owner)
+			await time.increase(ROUND_DURATION)
+			await _play(1, game, owner)
+			await _claim(game, owner)
+			await expect(_claim(game, owner)).to.be.revertedWith('You already claimed for this round')
+		})
+		
+		it("Entry price respected", async () => {
+			const { owner, game } = await loadFixture(deploy);
+			await expect(_play(2, game, owner, parseEther('1'))).to.be.revertedWith('Game price is not negociable')
+			await expect(_play(2, game, owner, parseEther('0.01'))).to.be.revertedWith('Game price is not negociable')
+			await expect(_play(2, game, owner, parseEther('0'))).to.be.revertedWith('Game price is not negociable')
+		})
 	})
 
-	it("Nothing to claim", async function () {
-		const { owner, game } = await loadFixture(deploy);
-		await _play(2, game, owner.address)
-		await expect(_claim(game, owner.address)).to.be.revertedWith('Nothing to claim')
-	})
-
-	it("No shares", async function () {
-		const { owner, game } = await loadFixture(deploy);
-		await expect(_claim(game, owner.address)).to.be.revertedWith('You have no share')
-	})
-
-	it("Already claimed", async function () {
-		const { owner, game } = await loadFixture(deploy);
-		await _play(2, game, owner.address)
-		await _play(1, game, owner.address)
-		await time.increase(ROUND_DURATION)
-		await _play(1, game, owner.address)
-		await _claim(game, owner.address)
-		await expect(_claim(game, owner.address)).to.be.revertedWith('You already claimed for this round')
-	})
 	
 })
