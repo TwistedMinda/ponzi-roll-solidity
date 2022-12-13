f7df427b42ecc6afc5d7d362bf4a96d996fe6b91
Twisted <julien.brunet.92@gmail.com>
2022-12-13 05:02:40 +0100

    added subid

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,10 +4,11 @@ const sleep = (duration: number) => new Promise(resolve => setTimeout(resolve, d
 
 async function main() {
 	const Game = await ethers.getContractFactory("Game");
+	const linkSubId = 2831;
 	const linkCoordinator = "0xAE975071Be8F8eE67addBC1A82488F1C24858067";
 	const hashKey = "0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f"
 	const contract = await Game.deploy(
-		2831,
+		linkSubId,
 		linkCoordinator,
 		hashKey
 	);
@@ -17,7 +18,7 @@ async function main() {
 	await sleep(30 * 1000)
 	await run("verify:verify", {
 		address: contract.address,
-		constructorArguments: [],
+		constructorArguments: [linkSubId, linkCoordinator, hashKey],
 	})
 	console.log(`✅ Contract verified`);
 }
